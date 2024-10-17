515ae371fe445571f78bf7e3ce5a85099b03334f
Filippo Gasparini <filippogaspa02@gmail.com>
2024-10-17 15:46:59 +0200

    ADD: primo esercizio completato

--- app.js
+++ app.js
@@ -12,4 +12,22 @@ console.log ('mail')
 //Se dobbiamo confrontare qualcosa che “cosa” ci serve?
 
 
-const = insertPrompt
+// lista degli utenti che possono accedere
+const names = ['riccardo@gmail.com' , 'fabrizio@gmail.com' , 'giovanni@gmail.com' , 'samuel@gmail.com' , 'luca@gmail.com' , 'sara@gmail.com']
+console.log (names)
+
+// chiedo all'utente la propria mail
+ const userName =  prompt('Inserisci la tua mail');
+
+// controllo se l'utente è nella lista
+
+if (names.includes (userName)) {
+    console.log(`Benvenuto, ${userName} sei stato invitato`)
+
+
+// se non è in lista 
+
+} else if (userName) {
+    console.log (`Mi dispiace! ${userName} non sei stato invitato.`)
+}
+
