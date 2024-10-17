7f6c91eabdab1e2d4c529b9a1e347156f05aad56
Filippo Gasparini <filippogaspa02@gmail.com>
2024-10-17 16:23:49 +0200

    ADD: primo commit al secondo esercizio

---
+++ dadi/script.js
@@ -0,0 +1,24 @@
+console.log ('Dadi')
+
+
+
+
+//Gioco dei dadi
+//Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+//Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+
+
+
+
+// scrivo i numeri da 1 a 6
+
+for (let i = 0; i <= 6; i++) {
+    console.log(i)
+
+// chiedo il numero nell'alert
+    const number1 = prompt ('Genera un numero per il giocatore')
+    const number2 = prompt ('Genera un numero per il computer')
+
+    let n= (math.random() *6)
+}
--- mail/app.js
+++ mail/app.js
@@ -4,12 +4,7 @@ console.log ('mail')
 //Mail
 //chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato
 //sull’esito del controllo.
-//Gioco dei dadi
-//Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-//Stabilire il vincitore, in base a chi fa il punteggio più alto.
-//Prima di partire a scrivere codice poniamoci qualche domanda:
-//Che ci sia un array da qualche parte?
-//Se dobbiamo confrontare qualcosa che “cosa” ci serve?
+
 
 
 // lista degli utenti che possono accedere
@@ -30,4 +25,3 @@ if (names.includes (userName)) {
 } else if (userName) {
     console.log (`Mi dispiace! ${userName} non sei stato invitato.`)
 }
-
